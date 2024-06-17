27935a063aae135f4d7c8d7dfe887e8dc7db9188
atlleung <andrewleung93@gmail.com>
2024-06-16 22:43:49 -0400

    Update pending status
    new pending status

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ function updateStatus() {
         .then(response => response.json())
         .then(data => {
             statusData = data;
-            if (statusData.main_status === 1) {
+            if (statusData.main_status === 2) {
                 statusDisplay.classList.remove('bg-danger'); // Remove Bootstrap bg-danger
+                statusDisplay.classList.remove('bg-warning'); // Remove Bootstrap bg-warning
                 statusDisplay.classList.add('bg-success');   // Add Bootstrap bg-success
-                statusText.textContent = statusData.status_text[1];
+                statusText.textContent = statusData.status_text[statusData.main_status];
+            } else if (statusData.main_status === 1){
+                statusDisplay.classList.remove('bg-success'); // Remove Bootstrap bg-success
+                statusDisplay.classList.remove('bg-danger'); // Remove Bootstrap bg-danger
+                statusDisplay.classList.add('bg-warning');     // Add Bootstrap bg-warning
+                statusText.textContent = statusData.status_text[statusData.main_status ];
             } else {
                 statusDisplay.classList.remove('bg-success'); // Remove Bootstrap bg-success
+                statusDisplay.classList.remove('bg-warning'); // Remove Bootstrap bg-warning
                 statusDisplay.classList.add('bg-danger');     // Add Bootstrap bg-danger
-                statusText.textContent = statusData.status_text[0];
+                statusText.textContent = statusData.status_text[statusData.main_status ];
             }
             latestUpdate.textContent = statusData.latest_update;
         })
